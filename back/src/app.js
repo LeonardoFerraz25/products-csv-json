a5ff402e4ae6e3ce639c7c2bf4021693e5a219d0
Leonardo Ferraz Novaes <leonardo.ferrazn@gmail.com>
2022-09-30 15:31:46 -0300

    feat:  configurando rotas e arquivo de csv

--- back/src/app.js
+++ back/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+const productRouter = require('./routes/productRoter');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/products', productRouter);
 
 module.exports = app;
